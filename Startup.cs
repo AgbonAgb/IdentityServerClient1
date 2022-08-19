3de00b542459596e72ce9314535ff6df32705c63
Godwin Agbon <agbonwinn@yahoo.com>
2022-08-19 15:35:04 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerClient1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication("Bearer", options =>
+                {
+                    options.ApiName = "inventoryapi";//the resource registerred in Server
+                    options.Authority = "https://localhost:44395/";//identity Server Url
+                });
+            services.AddAuthorization(x =>
+            {
+                x.AddPolicy("read",policy => policy.RequireClaim("scope", "inventoryapi.read"));
+                x.AddPolicy("write", policy => policy.RequireClaim("scope", "inventoryapi.write"));
+                x.AddPolicy("full", policy => policy.RequireClaim("scope", "inventoryapi.full"));
+
+            });
+
+
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
